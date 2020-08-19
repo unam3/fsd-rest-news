7c3bbb4b747ad81fb85e1a068fb263e120f8a2dc
Flowers Safety <40meta@gmail.com>
2020-08-19 22:49:10 +0600

    playing with hasql

--- src/RestNews.hs
+++ src/RestNews.hs
@@ -1,14 +1,42 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes #-}
 
 module RestNews
     ( runWarpWithLogger
     ) where
 
 import Control.Exception (bracket_)
+import Control.Monad (void)
+import Data.Int (Int64)
+import qualified Hasql.Connection as Connection
+--import qualified Hasql.Decoders as Decoders
+--import qualified Hasql.Encoders as Encoders
+import Hasql.Session (Session)
+import Hasql.Statement (Statement(..))
+import qualified Hasql.Session as Session
+import qualified Hasql.TH as TH
 import qualified Network.HTTP.Types as H
 import Network.Wai (Application, responseLBS)
 import Network.Wai.Handler.Warp (Port, run)
 import System.Log.Logger (Priority (DEBUG, ERROR), debugM, setLevel, traplogging, updateGlobalLogger)
+
+-- https://hackage.haskell.org/package/hasql-1.4.4
+-- https://github.com/nikita-volkov/hasql-tutorial1
+
+selectTest :: Session (Int64)
+selectTest = Session.statement ()
+    [TH.singletonStatement|
+        select ((2 :: int8) + (2 :: int8)) :: int8
+        |]
+
+dbCall :: IO ()
+dbCall = let {
+    connectionSettings = Connection.settings "localhost" 5432 "" "" "yay";
+} in do
+    connectionAcquired <- Connection.acquire connectionSettings
+    void . print $ case connectionAcquired of
+        Right connection -> Session.run selectTest connection
+        Left connectionError -> connectionError
 
 restAPI :: Application;
 restAPI request respond = bracket_
@@ -22,5 +50,4 @@ runWarp = let {
 } in run port restAPI
 
 runWarpWithLogger :: IO ()
-runWarpWithLogger = traplogging "rest-news" ERROR "Shutdown due to" $
-    (updateGlobalLogger "rest-news" (setLevel DEBUG) >> runWarp)
+runWarpWithLogger = dbCall
