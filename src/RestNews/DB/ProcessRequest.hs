e72bd031f7d50d15620ef1b6d484c3eef5d27d43
Flowers Safety <40meta@gmail.com>
2021-03-16 22:56:43 +0600

    getError tests was added

--- src/RestNews/DB/ProcessRequest.hs
+++ src/RestNews/DB/ProcessRequest.hs
@@ -2,6 +2,7 @@
 {-# LANGUAGE OverloadedStrings  #-}
 
 module RestNews.DB.ProcessRequest (
+    getError,
     createUser,
     deleteUser,
     getUser,
@@ -65,78 +66,21 @@ valueToUTFLBS :: Either Session.QueryError Value -> Either String ByteString
 valueToUTFLBS = bimap show encode
 
 getError :: Either Session.QueryError resultsType -> Maybe (String, Maybe String)
-{-
-    Left (
-        QueryError
-            "SELECT edit_article_draft($1 :: int4, $2 :: int4, $3 :: int4, $4 :: text, $5 :: text, $6 :: text, $7 :: text[], $8 :: int4[]) :: json"
-            ["1","9","2","\"PATCHEDttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttt\"","\"PATCHED\"","\"fs\"","[\"9\", \"2\"]","[1, 2]"]
-            (
-                ResultError
-                    (
-                        ServerError
-                            "22001"
-                            "value too long for type character varying(80)"
-                            Nothing
-                            Nothing
-                    )
-            )
-    )
--}
 getError (Left (Session.QueryError _ _ (Session.ResultError (Session.ServerError "22001" _ details _)))) =
     Just ("22001", fmap unpackChars details)
-{-
-Left (
-    QueryError
-        "WITH delete_results AS (DELETE FROM categories WHERE category_id = $1 :: int4 RETURNING *) SELECT CASE WHEN count(delete_results) = 0 THEN json_build_object('error', 'no such category') ELSE json_build_object('results', 'ook') END :: json FROM delete_results"
-        ["1"]
-        (
-            ResultError (
-                ServerError
-                    "23503"
-                    "update or delete on table \"categories\" violates foreign key constraint \"categories_parent_id_fkey\" on table \"categories\""
-                    (Just "Key (category_id)=(1) is still referenced from table \"categories\".")
-                    Nothing
-            )
-        )
-)
--}
+
 getError (Left (Session.QueryError _ _ (Session.ResultError (Session.ServerError "23503" _ details _)))) =
     Just ("23503", fmap unpackChars details)
-{-
-Left (
-    QueryError
-        "INSERT INTO users (username, password, name, surname, avatar, is_admin) VALUES ($1 :: text, crypt($2 :: text, gen_salt('bf', 8)), $3 :: text, $4 :: text, $5 :: text, FALSE) RETURNING json_build_object('user_id', user_id, 'name', name, 'surname', surname, 'avatar', avatar, 'creation_date', creation_date, 'is_admin', is_admin) :: json"
-        ["\"asdq\"","\"check, indeed\"","\"name\"","\"surname\"","\"asd\""]
-        (
-            ResultError (
-                ServerError
-                    "23505"
-                    "duplicate key value violates unique constraint \"users_username_key\""
-                    (Just "Key (username)=(asdq) already exists.")
-                    Nothing
-            )
-        )
-)
--}
+
 getError (Left (Session.QueryError _ _ (Session.ResultError (Session.ServerError "23505" _ details _)))) =
     Just ("23505", fmap unpackChars details)
-{-
-Left (
-    QueryError
-        "SELECT users.user_id :: int4, users.is_admin :: bool, COALESCE (authors.author_id, 0) :: int4 FROM users LEFT JOIN authors ON authors.user_id = users.user_id, (SELECT user_id FROM users WHERE username = lower($1 :: text) AND password = crypt($2 :: text, password)) AS matched_user WHERE users.user_id = matched_user.user_id"
-        ["\"usasdername5\"","\"12345\""]
-        (
-            ResultError (
-                UnexpectedAmountOfRows 0
-            )
-        )
-)
--}
+
 getError (Left (Session.QueryError _ _ (Session.ResultError (Session.UnexpectedAmountOfRows 0)))) =
     Just ("0", Nothing)
--- (Nothing,Left (QueryError "SELECT json_agg(users.*) :: json FROM users WHERE user_id = $1 :: int4" ["20"] (ResultError (RowError 0 UnexpectedNull))))
+
 getError (Left (Session.QueryError _ _ (Session.ResultError (Session.RowError 0 Session.UnexpectedNull)))) =
     Just ("0", Nothing)
+
 getError _ = Nothing
 
 getErrorCode :: Either Session.QueryError resultsType -> Maybe String
